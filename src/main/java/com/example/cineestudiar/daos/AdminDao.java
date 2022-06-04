d39bc4280a6f3a30ede1529696bee8c5290ba066
Marcelo Rojas PC <mrojasl@pucp.edu.pe>
2022-06-04 15:52:24 -0500

    beans daos

--- src/main/java/com/example/cineestudiar/daos/AdminDao.java
+++ src/main/java/com/example/cineestudiar/daos/AdminDao.java
@@ -1,2 +1,28 @@
-package com.example.cineestudiar.daos;public class AdminDao {
+package com.example.cineestudiar.daos;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class AdminDao {
+    public void obtenerSalasYSedes(){
+        try {
+            String user = "root";
+            String pass = "root";
+            String url = "jdbc:mysql://localhost:3306/mysystem4";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String sql = "";
+            Connection conn = DriverManager.getConnection(url,user,pass);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("");
+        } catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+    }
 }
